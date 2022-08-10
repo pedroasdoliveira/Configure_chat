c2e00353417ec9a079f9320d5f8f1a0c4a999fbb
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-10 15:29:54 -0300

    feat(server): implement connections with socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,30 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { getUsers, userJoin, userLeave } from "./utils/user";
 
-const app = express()
+const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server, {cors: {origin: "http://localhost:3000"}});
+const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
 
-io.on('connection', (socket) => {
-    console.log("Client connected");
-})
+io.on("connection", (socket) => {
+  socket.join("myChat");
 
-const port = 5000
-server.listen(port, () => console.log(`Server running on port ${port}.`))
+  socket.on("handle-connection", (username: string) => {
+    if (!userJoin(socket.id, username)) {
+      socket.emit("username-taken");
+    } else {
+      socket.emit("username-submitted-successfully");
+      io.to("myChat").emit("get-connected-users", getUsers());
+    }
+  });
+
+  socket.on("disconnect", () => {
+    userLeave(socket.id);
+  });
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`Server running on port ${port}.`));
